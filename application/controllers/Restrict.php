336a8fa0f6b3d874c516f0d10e72e3369e04e304
Matheus Moura <matheusmoura78@hotmail.com>
2022-02-18 01:37:31 -0300

    Tela de Login

--- application/controllers/Restrict.php
+++ application/controllers/Restrict.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Restrict extends CI_Controller {
 
     public function index() {
-        $this->template->show("restrict.php");
+        $this->template->show("login.php");
     }
 }
