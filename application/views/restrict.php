f6c7e42f9a5d800618e921f02d35fb92f57f7a6b
Matheus Moura <matheusmoura78@hotmail.com>
2022-02-20 10:07:03 -0300

    Botão de logoff e barra da navegação

--- application/views/restrict.php
+++ application/views/restrict.php
@@ -7,5 +7,36 @@
 						</div>
 					</div>
 				</div>
+
+                <div class="row">
+					<div class="col-lg-offset-5 col-lg-2 text-center">
+						<div class="form-group">
+                            <a class="btn btn-link"><i class="fa fa-user"></i></a>
+                            <a class="btn btn-link" href="restrict/logoff"><i class="fa fa-sign-out"></i></a>
+                        </div>
+					</div>
+				</div>
 			</div>
+
+            <div class="container">
+
+                <ul class="nav nav-tabs">
+                    <li class="active">
+                        <a href="#tab_courses" role="tab" data-toggle="tab">Cursos</a>
+                    </li>
+                    <li>
+                        <a href="#tab_team" role="tab" data-toggle="tab">Equipe</a>
+                    </li>
+                    <li>
+                        <a href="#tab_user" role="tab" data-toggle="tab">Usuários</a>
+                    </li>
+                </ul>
+
+                <div class="tab-content">
+                    <div id="tab_courses" class="tab-pane">Conteúdo Cursos</div>
+                    <div id="tab_team" class="tab-pane">Conteúdo Equipe</div>
+                    <div id="tab_user" class="tab-pane">Conteúdo Usuário</div>
+                </div>
+
+            </div>
 		</section>
--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,6 +1,6 @@
 <footer>
 			<div class="container text-center">
-				<p>Designed by <a href="http://moozthemes.com"><span>MOOZ</span>Themes.com</a></p>
+				<p>Template Designed by <a href="http://moozthemes.com"><span>MOOZ</span>Themes.com</a></p>
 			</div>
 		</footer>
 
